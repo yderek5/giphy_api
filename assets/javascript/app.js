7b8fdadfab93b65ef1c5e7b3a6a593f840e2121b
yderek5 <yderek5@gmail.com>
2017-10-25 00:13:29 -0500

    Add buttons and topic array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,30 @@
+var topicArray = ['Chris Pratt', 'Keanu Rheaves', 'DJ Khaled', 'Video Games', 'Coding',
+  'Kittens', 'Puppies', 'LOL', 'Dexter', 'Sons of Anarchy'
+];
+
+function makeButtons() {
+  $("#buttons").empty();
+  for (var b = 0; b < topicArray.length; b++) {
+    var newButton = $("<button>");
+    newButton.attr("data-person", topicArray[b]);
+    newButton.text(topicArray[b]);
+    $("#buttons").append(newButton);
+  }
+}
+makeButtons();
+
+$(".gif").on("click", function() {
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
+
 $("button").on("click", function() {
   var person = $(this).attr("data-person");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -8,6 +35,9 @@ $("button").on("click", function() {
       method: "GET"
     })
     .done(function(response) {
+      console.log(response);
+
+
       var results = response.data;
 
       for (var i = 0; i < results.length; i++) {
@@ -17,11 +47,14 @@ $("button").on("click", function() {
 
         var p = $("<p>").text("Rating: " + rating);
 
-        var personImage = $("<img>");
-        personImage.attr("src", results[i].images.fixed_height.url);
+        var gifImage = $("<img>");
+        gifImage.attr("src", results[i].images.fixed_height.url);
+        gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+        gifImage.addClass("gif");
 
         gifDiv.prepend(p);
-        gifDiv.prepend(personImage);
+        gifDiv.prepend(gifImage);
 
         $("#gifs-appear-here").prepend(gifDiv);
       }
